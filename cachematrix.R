617438bac7cdaf89dfa026246f4500b453cddc1f
Andres Betancourt <andresbetancourtrc@gmail.com>
2020-12-12 17:58:19 -0500

    creando las funciones para el archivo cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## creamos una funcion que guarde la matrix en cache
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(Matrix = matrix()){
+    #creamos una variable para hacer la inversa
+    M_inversa <- NULL
+    
+    M_set <- function(y){
+        Matrix <<- y
+        M_inversa <<- NULL
+    }
+    
+    #obtener la matrix original
+    M_Get <- function() Matrix
+    
+    #fijar la inversa
+    Set_Inv <- function(inversa) M_inversa <<- inversa
+    
+    #obtener la inversa
+    Get_Inv <- function() M_inversa
+    
+    #lista que retorna las 4 funciones
+    
+    list(M_set = M_set, 
+         M_Get = M_Get,
+         Set_Inv = Set_Inv,
+         Get_Inv = Get_Inv)
 }
 
+#funcion para hallar la inversa
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(M_inv, ...){
+    M_inversa <- M_inv$Get_Inv()
+    
+    if(!is.null(M_inversa)){
+        message("obteniendo datos en cache")
+        return(M_inversa)
+    }
+    
+    #Hallando la inversa
+    datos <- M_inv$M_Get()
+    M_inversa <- solve(datos, ...)
+    M_inv$Set_Inv(M_inversa)
+    M_inversa
 }
